223e779aebcad5f77f92367590762157815da10a
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-26 01:02:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;           // that will multiply the variables num1 and num2 whose results will equal "62".
+let random = Math.floor(Math.random() * 10) + 1;              // that will generate a random integer greater than 0.
+let mod = 29 % 5;                // set to an equation whose remainder is equal to "4".
+let max = Math.max(2,6,1,9,20,4);               // that will return "20" as the highest number in the set.
